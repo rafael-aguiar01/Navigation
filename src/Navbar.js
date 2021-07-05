4538a981f16a8397affb31f815839ac2ff579ce9
Rafael <rafael.a1@hotmail.com>
2021-07-05 16:42:39 -0300

    Create Nav Function

---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+export default function Navbar() {
+    const[navstatus, setnavstatus]=useState(0)
+    const[divwidth, setdivwidth]= useState('0px')
+
+    function opennav()
+    {
+        if(navstatus ==0)
+        {
+            setdivwidth('100%')
+            setnavstatus(1)
+        }else{
+            setdivwidth('0px')
+            setnavstatus(0)
+        }
+    }
+    return (
+        <div>
+            <div className='maindiv' style={{width:divwidth}}>
+                <button onClick={opennav}><i class="fas fa-bars"></i></button>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Serviços</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
